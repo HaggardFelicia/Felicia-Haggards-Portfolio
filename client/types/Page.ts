62052397fe5a62cba1f3c59170ab6646a43f334c
HaggardFelicia <haggardfelicia@outlook.com>
2024-06-23 13:08:13 -0500

    updating page type

--- client/types/Page.ts
+++ client/types/Page.ts
@@ -3,7 +3,10 @@ import { PortableTextBlock } from "next-sanity";
 export type Page = {
     _id: string;
     _createdAt: Date;
-    title: string;
+    name: string;
     slug: string;
+    image: string;
+    url: string;
     content: PortableTextBlock[];
+    alt: string;
 }
