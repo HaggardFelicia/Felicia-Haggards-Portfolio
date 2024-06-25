4a744f6780d4e0935f929e084598b979c45551b9
HaggardFelicia <haggardfelicia@outlook.com>
2024-06-24 23:39:26 -0500

    updating slug page

--- client/app/(site)/[slug]/page.tsx
+++ client/app/(site)/[slug]/page.tsx
@@ -12,42 +12,14 @@ type Props = {
 export default async function Page({params}: Props){
     // getting the page by the slug
     const page = await getPage(params.slug);
-
-    if(page.slug === "about"){
-        return (
-            <div>
-            <h1 className="bg-gradient-to-b from-red-900  via-red-700 to-rose bg-clip-text text-transparent font-bold text-5xl  drop-shadow">
-                    {page.title}
-                </h1>
-                <div className="text-lg text-gray mt-10">
-                    <PortableText value={page.content}/>
-                </div>
-            </div>
-        )
-    }
-    else if(page.slug === 'skills'){
         return(
             <div>
-                <h1 className="bg-gradient-to-b from-red-900  via-red-700 to-rose bg-clip-text text-transparent font-bold text-5xl  drop-shadow">
+                <h1 className="responsive-special-text">
                     {page.title}
                 </h1>
-                <div className="text-lg text-gray mt-10">
-                    {/* <Image src={page.image} alt={page.alt}/> */}
+                <div className="responsive-p-text text-rose margin4rem">
                     <PortableText value={page.content}/>
                 </div>
             </div>
         )
     }
-    else{
-        return(
-            <div>
-                <h1 className="bg-gradient-to-b from-red-900  via-red-700 to-rose bg-clip-text text-transparent font-bold text-5xl  drop-shadow">
-                    {page.title}
-                </h1>
-                <div className="text-lg text-gray mt-10">
-                    <PortableText value={page.content}/>
-                </div>
-            </div>
-        )
-    }
-}
